25070bf9af33705109665d06eaec0fd56c49e546
zzere <sebastiancamilo40@gmail.com>
2024-06-10 14:57:52 -0500

    seguridad implementado a clave y correo

--- SemilleroPagina/settings.py
+++ SemilleroPagina/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,9 +133,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # configuracion de email
 
+config = Config(".env.local")
+
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="uinfop@gmail.com"
-EMAIL_HOST_PASSWORD="psmu iwqd ezeb fwax"
+EMAIL_HOST_USER= config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD= config("EMAIL_HOST_PASSWORD")
